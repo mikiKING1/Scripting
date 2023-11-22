2bae0f95a50f736c9f5a034ae028104b4ce92abd
mikiKING1 <150952810+mikiKING1@users.noreply.github.com>
2023-11-22 18:58:44 +0100

    np-c

---
+++ NPC.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // NPC Health
+    public int level = 1;
+
+    //NPC Level 
+    public int leavel = 1;
+
+    //NPC Speed
+    public float speed = 1.2f;
+
+    void Start()
+    {
+
+    }
+     ...
+}
